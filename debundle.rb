6baa713ab5af894304c7c9670a5f0345c8ee5125
Jan Lelis <mail@janlelis.de>
2015-04-02 09:15:49 +0200

    load Kernel#gem instead of redefining it

--- debundle.rb
+++ debundle.rb
@@ -16,21 +16,10 @@ module Debundle
       Gem.clear_paths
       Gem::Specification.reset
       load 'rubygems/core_ext/kernel_require.rb'
-      remove_bundler_monkeypatches
+      load 'rubygems/core_ext/kernel_gem.rb'
     end
   rescue => e
     puts "Debundling failed: #{e.message}"
-  end
-
-  def self.remove_bundler_monkeypatches
-    Kernel.module_eval do
-      def gem(gem_name, *requirements) # :doc:
-        skip_list = (ENV['GEM_SKIP'] || "").split(/:/)
-        raise Gem::LoadError, "skipping #{gem_name}" if skip_list.include? gem_name
-        spec = Gem::Dependency.new(gem_name, *requirements).to_spec
-        spec.activate if spec
-      end
-    end
   end
 end
 
