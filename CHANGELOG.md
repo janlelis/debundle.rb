923a8d277da3ea27a9949a57852054a1a7f88bcd
Jan Lelis <mail@janlelis.de>
2016-07-25 12:51:35 +0200

    Bundler 1.12+ way of preserving ENV, use 1.0 for older bundler and 0.x.d for even older ones
    Also see: https://github.com/ConradIrwin/pry-debundle/issues/12

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+
+## 1.1.0
+
+- Bundler 1.12+ way of preserving ENV, use 1.0 for older bundler and 0.x.d for even older ones
+
+## 1.0.0
+
+- Modern version of debundle.rb without backward compatibility
+
+## < 1.0 (0.x.d)
+
+- Fork of pry-debundle
--- debundle.rb
+++ debundle.rb
@@ -5,11 +5,11 @@
 # Copyright (c) Jan Lelis <mail@janlelis.de>
 
 module Debundle
-  VERSION = '1.0.0'
+  VERSION = '1.1.0.pre'
 
   def self.debundle!
     if Gem.post_reset_hooks.reject!{ |hook| hook.source_location.first =~ %r{/bundler/} }
-      Bundler.preserve_gem_path
+      ENV.replace(Bundler::EnvironmentPreserver.new(ENV, %w(GEM_PATH)).backup) if defined? Bundler::EnvironmentPreserver
       Gem.clear_paths
       load 'rubygems/core_ext/kernel_require.rb'
       load 'rubygems/core_ext/kernel_gem.rb'
